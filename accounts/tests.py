87713b420ec56e53db868fb92ae7f261a2a1c59d
Great83g <polevaya376@gmail.com>
2025-12-19 09:09:19 +0500

    Add web login page template and test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginPageTests(TestCase):
+    def test_login_page_renders(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/login.html")
