7c6670889345a68b7b0a599541f68222e06c056a
Great83g <polevaya376@gmail.com>
2026-04-20 11:25:17 +0500

    Add safe _forecast_value_to_kw helper for forecast list

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,7 @@ app_name = "dashboard"
 
 urlpatterns = [
     path("", views.station_list, name="station-list"),
+    path("about/", views.about_company, name="about-company"),
 
     path("station/create/", views.station_create, name="station-create"),
     path("station/<int:pk>/edit/", views.station_edit, name="station-edit"),
