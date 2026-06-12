4ba015802b4d99e703cb3cbe01c39544faa0e982
Great83g <polevaya376@gmail.com>
2026-06-12 14:06:53 +0500

    Add Virtual ESS scaffold

---
+++ virtual_ess/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = "virtual_ess"
+
+urlpatterns = [
+    path("", views.station_list, name="station-list"),
+    path("station/<int:station_id>/settings/", views.station_settings, name="station-settings"),
+    path("station/<int:station_id>/simulate/", views.station_simulate, name="station-simulate"),
+]
